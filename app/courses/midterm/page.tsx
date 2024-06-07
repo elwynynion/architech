222a250ca5ae97931fc393669aa80cf20dbb5b71
Jericho <jericho.caneso13@gmail.com>
2024-06-08 00:33:57 +0800

    midtermpage

--- app/courses/midterm/page.tsx
+++ app/courses/midterm/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navigation  from '@/components/shared/Navigation'
 
 function page() {
   return (
-    <div>
-      
-    </div>
+    <main className="bg-[#FFFEFA] h-[100vh]">
+      <div className="container">
+      <Navigation/>
+        
+        <div className="border border-[#4F7853] rounded-lg ">
+        <h1>DAMN</h1>
+        
+
+        </div>
+      </div>
+    </main>
+    
   )
 }
 
